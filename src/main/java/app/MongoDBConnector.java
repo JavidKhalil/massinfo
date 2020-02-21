8f3ce09b0930dc17e918def4614a6d672ec7b5bf
JavidKhalil <cavidxalilov@gmail.com>
2020-02-21 11:14:22 +0200

    _

---
+++ src/main/java/app/MongoDBConnector.java
@@ -0,0 +1,14 @@
+package app;
+
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+public class MongoDBConnector {
+
+    public static MongoCollection<Document> collection;
+
+    static {
+
+    }
+
+}
